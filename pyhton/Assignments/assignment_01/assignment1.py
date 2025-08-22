db04a741d3e883cb67a0ccefede4d480659cfb26
Hafiz_Muhammad_Umair <163179440+Umair231183@users.noreply.github.com>
2025-08-22 22:37:50 +0500

    Assignments of pyhton

---
+++ pyhton/Assignments/assignment_01/assignment1.py
@@ -0,0 +1,8 @@
+Number1 = input("Enter 1st Number" )
+Number2 = input("Enter 2nd Number") 
+operator = '+''-''*''/'
+
+if(Number1  == '+' ):
+    print(f'\t\n operation perform by user is addition ${Number1} + ${Number2}')
+else:
+    print(f'\t\n ')
